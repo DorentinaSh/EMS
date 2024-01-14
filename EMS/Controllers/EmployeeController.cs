0006d9341f01d1dea789f7aefc6ab9a8fac73d01
Dorentina Shabani <dorentina.shabani@ikubinfo.al>
2024-01-14 23:32:29 +0100

    Add endpoints update to show the view and also on submit save the edited employee

--- EMS/Controllers/EmployeeController.cs
+++ EMS/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using EMS.Core.Contract;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using EMS.Core.Contract.Employee.Request;
+using AutoMapper;
 
 namespace EMS.Controllers;
 
@@ -12,11 +13,13 @@ public class EmployeeController : Controller
 {
     private readonly IEmployeeService _employeeService;
     private readonly IPositionService _positionService;
+    private readonly IMapper _mapper;
 
-    public EmployeeController(IEmployeeService employeeService, IPositionService positionService)
+    public EmployeeController(IEmployeeService employeeService, IPositionService positionService, IMapper mapper)
     {
         _employeeService = employeeService;
         _positionService = positionService;
+        _mapper = mapper;
     }
 
     [HttpGet]
@@ -49,10 +52,27 @@ public class EmployeeController : Controller
         return RedirectToAction("Index");
     }
 
-    [HttpPut]
-    public async Task<ActionResult<bool>> Update(UpdateEmployeeCommand updateEmployeeCommand, CancellationToken cancellationToken)
+    [HttpGet("{id}")]
+    public async Task<ActionResult> Update(Guid id, CancellationToken cancellationToken)
     {
-        return Ok(await _employeeService.UpdateEmployee(updateEmployeeCommand, cancellationToken));
+        var positions = await _positionService.GetPositions(cancellationToken);
+        var mappedPositions = positions.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).ToList();
+        ViewData["Positions"] = mappedPositions;
+
+        var employee = await _employeeService.GetEmployeeById(id, cancellationToken);
+
+        var mappedEmployee = _mapper.Map<UpdateEmployeeCommand>(employee);
+
+        return View(mappedEmployee);
+    }
+
+    [HttpPost("{id}")]
+    public async Task<ActionResult> Update(Guid id, [FromForm] UpdateEmployeeCommand updateEmployeeCommand, CancellationToken cancellationToken)
+    {
+        updateEmployeeCommand.Id = id;
+        await _employeeService.UpdateEmployee(updateEmployeeCommand, cancellationToken);
+
+        return RedirectToAction("Index");
     }
 
     [HttpPost("{id}")]
